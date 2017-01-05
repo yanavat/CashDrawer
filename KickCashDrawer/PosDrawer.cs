fb5cf16ba8f9c22fa8a1c47a2cb3a8672e8add38
Yanavat <hayseventyone@gmail.com>
2017-01-06 00:30:18 +0700

    fist time sync
    dddd

---
+++ KickCashDrawer/PosDrawer.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.PointOfService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KickCashDrawer
+{
+    class PosDrawer
+    {
+
+        CashDrawer myCashDrawer;
+        PosExplorer explorer;
+
+        public void CashDrawer()
+        {
+            explorer = new PosExplorer();
+            DeviceInfo ObjDevicesInfo = explorer.GetDevice("CashDrawer");
+            myCashDrawer = (CashDrawer)explorer.CreateInstance(ObjDevicesInfo);
+        }
+
+        public void OpenCashDrawer(int interval )
+        {
+            myCashDrawer.Open();
+            myCashDrawer.Claim(interval);
+            myCashDrawer.DeviceEnabled = true;
+            myCashDrawer.OpenDrawer();
+            myCashDrawer.DeviceEnabled = false;
+            myCashDrawer.Release();
+            myCashDrawer.Close();
+        }
+    }
+}
---
+++ KickCashDrawer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KickCashDrawer
+{
+    class program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int  Interval = 1000;
+            try
+            {
+                if  (args[0] == "-k")
+                {
+                    if (args.Length > 1)  
+                        Int32.TryParse(args[1], out Interval);
+                    PosDrawer CashDrawer = new PosDrawer();
+                    CashDrawer.CashDrawer();
+                    CashDrawer.OpenCashDrawer(Interval);
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Error Invalid Parameter");
+            }
+        }
+
+    }
+
+
+}
+
